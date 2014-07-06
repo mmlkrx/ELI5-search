59174a26066bdd2e6620b525c557056a463869f0
Matthias Loker <mmloker@gmail.com>
2014-07-06 15:55:37 -0400

    sets up somewhat decent looking css

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -3,11 +3,20 @@ class AppController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
+
+    if params[:user_query]
+      @query = params[:user_query]
+    end
+
     erb :'index'
   end
 
-  post '/' do
+  post '/search' do
+    @query = params[:user_query]
 
+    # How we want to access our search results
+    # @result = RedditScraper.new(@query)
+    erb :'search_result'
   end
 
 end
